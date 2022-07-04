c3dd72dc2c42f14d46c8c15fb0f7d02566675db4
Enrique Saracho Felix <esarachof97@gmail.com>
2022-07-04 17:19:10 -0600

    Update and Create .

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Navbar() {
+    return (
+        <nav className="header">
+            <div className="logo">
+                <img src=""/>
+                <h2>ReactFacts</h2>
+            </div>
+            <h3 className="extra-title">React Course - Project 1</h3>
+        </nav>
+    )
+}
