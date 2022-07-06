fb2f8efc7749ba99c5ca5bd00297972546033351
Enrique Saracho Felix <esarachof97@gmail.com>
2022-07-05 18:30:44 -0600

    Update src/components/Navbar.js src/style.css
    Updated Navbar.js and style.css files, stylized navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,14 @@
 import React from "react";
+import reactLogo from "../images/react-icon-small.png";
 
 export default function Navbar() {
-    return (
-        <nav className="header">
-            <div className="logo">
-                
-                <h2>ReactFacts</h2>
-            </div>
-            <h3 className="extra-title">React Course - Project 1</h3>
-        </nav>
-    )
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <img src={reactLogo} alt="" className="react-logo"/>
+        <h2>ReactFacts</h2>
+      </div>
+      <h3 className="extra-title">React Course - Project 1</h3>
+    </nav>
+  );
 }
